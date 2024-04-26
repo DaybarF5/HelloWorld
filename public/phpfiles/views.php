d7467a89fd4282e1a97d100094b322f5ea0a1d38
Daniel Aybar <daybar@FACTORCINCO.LOCAL>
2024-04-26 13:56:15 +0200

    cambios

--- public/phpfiles/views.php
+++ public/phpfiles/views.php
@@ -22,17 +22,17 @@ if (empty($_SESSION['usuario']) && $_SESSION['screen'] != 'REGISTRO') {
     $_SESSION['screen'] = 'LOGIN';
 }
 
-if (isset($_POST['conecta']) && $_POST['conecta'] == '1') { //boton para llevarte al formulario de conectar
+if (isset($_POST['conecta']) && $_POST['conecta'] == '1') { //boton para llevarte al formulario de login
     $_SESSION['screen'] = 'LOGIN';
 }
 //--------------------------------------------------------------------------------------------
-if (isset($_POST['anadirEmp']) && $_POST['anadirEmp'] == '1') { //boton que te lleva hacia atras
+if (isset($_POST['anadirEmp']) && $_POST['anadirEmp'] == '1') { //boton para llevarte al formulario de añadir empleados
     $_SESSION['screen'] = 'ANADIR_EMP';
 }
 if (isset($_POST['register']) && $_POST['register'] == '1') { //boton para llevarte al formulario de registrarte
     $_SESSION['screen'] = 'REGISTRO';
 }
-if (isset($_POST['back']) && $_POST['back'] == '1') { //boton para llevarte al formulario de registrarte
+if (isset($_POST['back']) && $_POST['back'] == '1') { //boton para ir hacia atras
     $_SESSION['screen'] = 'DASHBOARD';
 }
 
